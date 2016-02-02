63efd0e43e7c3d8d5a527a86a9f87af7c896f57b
acuynet <amelie.cuynet@univ-smb.fr>
2016-02-02 15:24:49 +0100

    programme

---
+++ essai_fonction.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 02 15:22:55 2016
+Projet collaboratif
+@author: Amelie, Leonida and Amandine
+"""
+
+#Une fonction
+
+def fonc(x,y):
+  
+  
+#Affichage
